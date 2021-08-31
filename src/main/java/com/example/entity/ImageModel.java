b1da5bc2cea78f1e6309534f5b6fc36a1450b705
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-08-31 08:47:00 +0300

    Fields for Images added

--- src/main/java/com/example/entity/ImageModel.java
+++ src/main/java/com/example/entity/ImageModel.java
@@ -1,4 +1,11 @@
 package com.example.entity;
 
 public class ImageModel {
+
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
+
 }
