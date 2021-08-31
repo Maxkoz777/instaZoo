ed4aa2389c00cb66880e4ba638393b5c3c1e0da8
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-08-31 08:55:13 +0300

    Fields for Comment added

--- src/main/java/com/example/entity/Comment.java
+++ src/main/java/com/example/entity/Comment.java
@@ -1,4 +1,20 @@
 package com.example.entity;
 
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class Comment {
+
+    private Long id;
+    private Post post;
+    private String userName;
+    private Long userId;
+    private String message;
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = LocalDateTime.now();
+    }
+
 }
