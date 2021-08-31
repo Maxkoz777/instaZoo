fd2976aaf06bd472cddef7de186b1b3dc2f34d84
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-08-31 08:30:38 +0300

    Initial commit

---
+++ src/main/java/com/example/instazoo/InstaZooApplication.java
@@ -0,0 +1,13 @@
+package com.example.instazoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaZooApplication.class, args);
+    }
+
+}
