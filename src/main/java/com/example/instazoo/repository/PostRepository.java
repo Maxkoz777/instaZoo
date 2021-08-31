d218a255aa22ac126f5b265fd0da4904c92a799a
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-08-31 10:26:52 +0300

    PostRepository added

---
+++ src/main/java/com/example/instazoo/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package com.example.instazoo.repository;
+
+import com.example.instazoo.entity.Post;
+import com.example.instazoo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+
+    List<Post> findAllByOrderByCreatedDateDesc();
+
+    Optional<Post> findPostByIdAndUser(Long id, User user);
+
+}
