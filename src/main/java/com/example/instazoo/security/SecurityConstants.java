e299711cf2a1bceb6d00ecaf7f53daede9b3089f
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-09-09 11:58:58 +0300

    Security instantiation

---
+++ src/main/java/com/example/instazoo/security/SecurityConstants.java
@@ -0,0 +1,19 @@
+package com.example.instazoo.security;
+
+import org.springframework.format.number.PercentStyleFormatter;
+
+public class SecurityConstants {
+
+    public static final String SIGN_UP_URLS = "/api/auth/**";
+
+    public static final String SECRET = "SecretKeyJWT";
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    public static final String HEADER_STRING = "Authorization";
+
+    public static final String CONTENT_TYPE = "application/json";
+
+    public static final long EXPIRATION_TIME = 600_000; // 10 min
+
+}
