f1ff553a7bbd74fc94ed58797ca372e2f844bbd7
m.kozhinov <m.kozhinov@tinkoff.ru>
2021-09-18 12:47:03 +0300

    User facade added

---
+++ src/main/java/com/example/instazoo/facade/UserFacade.java
@@ -0,0 +1,20 @@
+package com.example.instazoo.facade;
+
+import com.example.instazoo.dto.UserDTO;
+import com.example.instazoo.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFacade {
+
+    public UserDTO userToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstname(user.getName());
+        userDTO.setLastname(user.getLastname());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setBio(user.getBio());
+        return userDTO;
+    }
+
+}
